5c75b076a8cb57bf2967f1796cc73e26feb912c5
brother nifty macbook air <brotherniftymacbookair@brothers-MacBook-Air.local>
2025-04-07 20:21:13 -0400

    this

--- manning/oauth-in-action-code-master/exercises/ch-3-ex-1/authorizationServer.js
+++ manning/oauth-in-action-code-master/exercises/ch-3-ex-1/authorizationServer.js
@@ -160,8 +160,8 @@ app.post("/token", function (req, res) {
   var auth = req.headers["authorization"];
   if (auth) {
     // check the auth header
-    var clientCredentials = new Buffer(auth.slice("basic ".length), "base64")
-      .toString()
+    var clientCredentials = Buffer.from(auth.slice("basic ".length), "base64")
+      .toString("utf8")
       .split(":");
     var clientId = querystring.unescape(clientCredentials[0]);
     var clientSecret = querystring.unescape(clientCredentials[1]);
