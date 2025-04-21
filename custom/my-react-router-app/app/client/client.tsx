dfc60531c94a06fef0841953a4acde6ff2b35e15
brother nifty macbook air <brotherniftymacbookair@brothers-MacBook-Air.local>
2025-04-21 14:03:32 -0400

    this

---
+++ custom/my-react-router-app/app/client/client.tsx
@@ -0,0 +1,11 @@
+export function Client() {
+  return (
+    <div className="jumbotron">
+      <h1>Client</h1>
+      {/* <p>Access token value: <span class="label label-danger"><%- access_token ? access_token : 'NONE' %></span></p>
+		  <p>Scope value: <span class="label label-danger"><%- scope ? scope : 'NONE' %></span></p>
+		  <a class="btn btn-default" href="/authorize">Get OAuth Token</a> 
+		  <a class="btn btn-default" href="/fetch_resource">Get Protected Resource</a>  */}
+    </div>
+  );
+}
--- custom/my-react-router-app/app/routes/home.tsx
+++ custom/my-react-router-app/app/routes/home.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/client";
+import { Client } from "../client/client";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Client />;
 }
