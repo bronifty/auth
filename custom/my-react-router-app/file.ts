124864ade46096c4f044f3489425c523b190f80b
brother nifty macbook air <brotherniftymacbookair@brothers-MacBook-Air.local>
2025-04-30 18:04:40 -0500

    moving react context to rr2 project and using it as the model for xstate singleton on the server

--- custom/my-react-router-app/file.ts
+++
@@ -1,4 +0,0 @@
-import config from "./config.json" with { type: "json" };
-
-
-console.log("config.json", config, `\nconfig in grave quotes: ${JSON.stringify(config, null, 2)};`, `\nconfig.mode: ${config.mode};`);
