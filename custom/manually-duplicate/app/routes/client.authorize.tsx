7957890b2ed5cd1779f9cc5f5dc03cf357027ab8
brother nifty macbook air <brotherniftymacbookair@brothers-MacBook-Air.local>
2025-05-07 16:38:12 -0500

    this

--- custom/manually-duplicate/app/routes/client.authorize.tsx
+++ custom/manually-duplicate/app/routes/client.authorize.tsx
@@ -1,16 +1,17 @@
 import { redirect } from "react-router";
-
+import randomstring from "randomstring";
+import url from "url";
 const authServer = {
-  authorizationEndpoint: "http://localhost:9001/authorize",
-  tokenEndpoint: "http://localhost:9001/token",
+  authorizationEndpoint: "http://localhost:5173/server/authorize",
+  tokenEndpoint: "http://localhost:5173/server/token",
 };
 
-const protectedResource = "http://localhost:9002/resource";
+const protectedResource = "http://localhost:5173/api/resource";
 
 const client = {
   client_id: "oauth-client-1",
   client_secret: "oauth-client-secret-1",
-  redirect_uris: ["http://localhost:9000/callback"],
+  redirect_uris: ["http://localhost:5173/client/callback"],
 };
 
 let state = null;
@@ -18,6 +19,34 @@ let state = null;
 let access_token = null;
 let scope = null;
 
+var buildUrl = function (base, options, hash) {
+  var newUrl = url.parse(base, true);
+  delete newUrl.search;
+  if (!newUrl.query) {
+    newUrl.query = {};
+  }
+  Object.keys(options).forEach(function (key) {
+    newUrl.query[key] = options[key];
+  });
+  if (hash) {
+    newUrl.hash = hash;
+  }
+
+  return url.format(newUrl);
+};
+
 export async function loader() {
-  return redirect("/server/authorize");
+  access_token = null;
+
+  state = randomstring.generate();
+
+  var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+    response_type: "code",
+    client_id: client.client_id,
+    redirect_uri: client.redirect_uris[0],
+    state,
+  });
+
+  console.log("redirect", authorizeUrl);
+  return redirect(authorizeUrl);
 }
---
+++ custom/manually-duplicate/temp.js
@@ -0,0 +1,2 @@
+http://localhost:5173/server/authorize?response_type=code&client_id=oauth-client-1&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fclient%2Fcallback&state=v1hn9EyzPkz5U4BfdKBcbBnJVA7BwLx5
+
